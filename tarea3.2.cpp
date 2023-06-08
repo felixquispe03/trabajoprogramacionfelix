f68e3b356e3e0b49f6f130ce8cc0b03e4f72ea05
GAC <129469688+Gretly@users.noreply.github.com>
2023-06-08 10:52:14 -0500

    ACTUALIZAR

--- tarea3.2.cpp
+++ tarea3.2.cpp
@@ -8,4 +8,4 @@ int main() {
    cout << "El doble de " << numero << " es: " << numero*2 << endl;
    cout << "El triple de " << numero << " es: " << numero*3 << endl;
    return 0;
-}
+} // CALCULAR TAMBIEN QUE SEGUN  SEA EL RESULTADO, ENVIE SI ESE NUMERO ES PAR O IMPAR
